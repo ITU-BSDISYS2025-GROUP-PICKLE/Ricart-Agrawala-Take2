161d14001760caa896b529f311341468395b3ead
oshe <oshe@itu.dk>
2025-11-27 18:27:11 +0100

    Remaining implementation 😬
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- node/node.go
+++ node/node.go
@@ -19,7 +19,7 @@ import (
 type Node struct {
 	pb.UnimplementedNodeServer
 
-	port  string   // Serves as both the localhost port and ID
+	port  int32    // Serves as both the localhost port and ID
 	peers []string // Slice of peer-ports
 
 	mu      sync.Mutex
@@ -27,13 +27,16 @@ type Node struct {
 	lamport int32  // Lamport timestamp
 	replies int    // Number of received replies
 
-	queued_replies []string // Slice of peer-ports to reply to after leaving CS
+	queued_replies []string // Slice of peer addresses to reply to after leaving CS
 }
 
 // Start a node server. Runs indefinitely
 func (n *Node) StartServer() {
+	// Convert node's port (back to) a string
+	nPort := strconv.Itoa(int(n.port))
+
 	// Create address
-	address := "localhost:" + n.port
+	address := "localhost:" + nPort
 
 	// Create listener
 	lis, err := net.Listen("tcp", address)
@@ -54,16 +57,31 @@ func (n *Node) StartServer() {
 	}
 }
 
-// Request CS access from all peers
+// Request CS access from all peers. Lecture 7 slide 15/50 'on enter do' part
 func (n *Node) RequestCSFromPeers() {
+	// Change state and increase lamport timestamp
+	n.mu.Lock()
+	n.state = "WANTED"
+	n.lamport++
+	log.Printf("Node #%d [T:%d] is requesting access to the critical section", n.port, n.lamport)
+	n.mu.Unlock()
+
+	// Start a goroutine requesting CS from every peer
 	for _, peer := range n.peers {
 		go n.RequestCSFromPeer(peer)
 	}
 
+	// Wait for replies from all peers...
 	for n.replies < len(n.peers) {
-		// Wait for replies from all peers...
+		//time.Sleep(time.Second) // Check once per second instead of thousands of times
 	}
 
+	//Change state
+	n.mu.Lock()
+	n.state = "HELD"
+	n.mu.Unlock()
+
+	// Enter and eventually leave the critical section
 	n.EnterAndLeaveCS()
 }
 
@@ -75,22 +93,15 @@ func (n *Node) RequestCSFromPeer(peer_port string) {
 	// Create connection
 	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("Connect to server failed: %v", err)
+		log.Fatalf("Connecting failed: %v", err)
 	}
 
 	// Create intermediate peer client
 	peer := pb.NewNodeClient(conn)
 
 	// Create CS request
-	portInt, err := strconv.Atoi(n.port)
-	if err != nil {
-		log.Fatalf("Node-port conversion failed: %v", err)
-	}
-
-	portInt32 := int32(portInt)
-
 	req := &pb.CSRequest{
-		NodePort: portInt32,
+		NodePort: n.port,
 		Lamport:  n.lamport,
 	}
 
@@ -99,28 +110,95 @@ func (n *Node) RequestCSFromPeer(peer_port string) {
 	if err != nil {
 		log.Fatalf("ReceiveCSRequest failed: %v", err)
 	}
-
-	// TODO: This functionality goes in RespondToCSRequest(...)
-	// Increment the requesting node's amount of received replies
-	// n.mu.Lock()
-	// n.replies++
-	// n.mu.Unlock()
 }
 
 // RPC function
 func (n *Node) ReceiveCSRequest(_ context.Context, req *pb.CSRequest) (*pb.Empty, error) {
-	// TODO: Implement
-	return nil, nil
+	// Create address
+	reqPort := strconv.Itoa(int(req.NodePort))
+	address := "localhost:" + reqPort
+
+	// Lecture 7 slide 15/50 'on receive do' part
+	if (n.state == "HELD") || (n.state == "WANTED" && n.IsLessThanPeer(req)) {
+		// Queue reply
+		n.queued_replies = append(n.queued_replies, address)
+		log.Printf("Node #%d [T:%d] queued responding to Node #%s [T:%d]", n.port, n.lamport, reqPort, req.Lamport)
+	} else {
+		// Respond directly
+		n.RespondToCSRequest(address)
+		log.Printf("Node #%d [T:%d] responded directly to Node #%s [T:%d]", n.port, n.lamport, reqPort, req.Lamport)
+	}
+
+	// Upon receiving a CS request, update Lamport timestamp
+	n.mu.Lock()
+	n.lamport = max(n.lamport, req.Lamport) + 1
+	n.mu.Unlock()
+
+	return &pb.Empty{}, nil
+}
+
+// Return whether a node is 'less than' another.
+// Primarily determined by Lamport timestamp, otherwise port-number
+func (n *Node) IsLessThanPeer(peerReq *pb.CSRequest) bool {
+	if n.lamport == peerReq.Lamport {
+		return n.port < peerReq.NodePort
+	}
+
+	return n.lamport < peerReq.Lamport
 }
 
 // Respond to a received CS request
-func (n *Node) RespondToCSRequest(peer string) {
-	// TODO: Implement
+func (n *Node) RespondToCSRequest(address string) {
+	// Create connection
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Connect to server failed: %v", err)
+	}
+
+	// Create intermediate peer client
+	peer := pb.NewNodeClient(conn)
+
+	// Create CS response
+	resp := &pb.CSResponse{
+		NodePort: n.port,
+		Lamport:  n.lamport,
+	}
+
+	// Increment the node-we're-responding-to's number of received replies
+	peer.IncrementReceived(context.Background(), resp)
+}
+
+// RPC function
+func (n *Node) IncrementReceived(_ context.Context, resp *pb.CSResponse) (*pb.Empty, error) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	n.replies++
+	log.Printf("Node #%d [T:%d] received a reply from Node #%d [T:%d]", n.port, n.lamport, resp.NodePort, resp.Lamport)
+
+	return &pb.Empty{}, nil
 }
 
 // Simulate entering the critical section
 func (n *Node) EnterAndLeaveCS() {
-	// TODO: Implement
+	// Enter and do something cool in the critical section
+	log.Printf("Node #%d [T:%d] entered the critical section 🚨🐔", n.port, n.lamport)
+	time.Sleep(3 * time.Second)
+	log.Printf("Node #%d [T:%d] left the critical section 😴💭🐑", n.port, n.lamport)
+
+	// Lecture 7 slide 15/50 'on exit do' part
+	n.mu.Lock()
+	n.state = "RELEASED"
+	n.mu.Unlock()
+	for _, address := range n.queued_replies {
+		n.RespondToCSRequest(address)
+	}
+
+	// Reset queued replies and number of received replies
+	n.mu.Lock()
+	n.queued_replies = []string{}
+	n.replies = 0
+	n.mu.Unlock()
 }
 
 func main() {
@@ -131,13 +209,19 @@ func main() {
 		return
 	}
 
-	// Parse arguments
-	nPort := os.Args[1]
+	// Parse port argument
+	nPort, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Port-parsing failed: %v", err)
+	}
+	nPort32 := int32(nPort)
+
+	// Parse peer argument(s)
 	nPeers := os.Args[2:]
 
-	// Create node
+	// Create node. Lecture 7 slide 15/50 'on initialisation do' part
 	n := &Node{
-		port:    nPort,
+		port:    nPort32,
 		peers:   nPeers,
 		state:   "RELEASED",
 		lamport: 0,
@@ -146,9 +230,14 @@ func main() {
 	// Start server
 	go n.StartServer()
 
-	// TODO: Request CS a few times
+	// Give time for the server to start
+	time.Sleep(3 * time.Second)
 
-	// Shut down after 2 minutes
-	time.Sleep(2 * time.Minute)
+	// Request CS a few times
+	for range 5 {
+		n.RequestCSFromPeers()
+	}
+
+	// Shut down server once every node has entered the CS a few times
 	log.Println("Server shutting down...")
 }
